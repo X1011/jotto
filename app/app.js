f571310fb42983175273cb2d8882f659a5120d43
X1011 <xx1011@gmail.com>
2014-09-29 09:01:02 +0000

    basic guess UI

--- app/app.js
+++ app/app.js
@@ -1,2 +1,11 @@
-'use strict';
-
+angular.module('jotto', [])
+	.controller('game', function($scope) {
+		$scope.word = 'word';
+		$scope.clues = [];
+		
+		$scope.submitGuess = function() {
+			$scope.clues.push($scope.guess);
+			$scope.guess = '';
+		};
+	})
+;
