bd2d2745c09db82f63ed40b326d66802a23c4432
X1011 <xx1011@gmail.com>
2014-10-04 05:45:04 +0000

    multiplayer with PubNub

--- app/app.js
+++ app/app.js
@@ -1,12 +1,30 @@
 angular.module('jotto', [])
 	.controller('game', function($scope) {
-		$scope.word = 'word';
+	
+		var channel = 'clues';
+		$scope.word = 'hacks';
 		$scope.clues = [];
 		
+		var pubnub = PUBNUB.init({
+			publish_key:   'pub-c-8e12032f-3923-40df-a70d-ed394dba494d',
+			subscribe_key: 'sub-c-386d444c-2db1-11e4-8788-02ee2ddab7fe'
+		});
+	
+		pubnub.subscribe({
+			channel: channel,
+			message: function(clue) {
+				$scope.$apply(function() {
+					$scope.clues.unshift(clue);
+				});
+			}
+		});
+		
 		$scope.submitGuess = function() {
-			$scope.clues.unshift({
-				word: $scope.guess,
-				score: score($scope.word, $scope.guess)
+			pubnub.publish({channel: channel,
+				message: {
+					word: $scope.guess,
+					score: score($scope.word, $scope.guess)
+				}
 			});
 			$scope.guess = '';
 		};
